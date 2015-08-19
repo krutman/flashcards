0bd745f938aea4aa60068aea710cdaae159275cd
Misha <mikhail.krutman@gmail.com>
2015-08-19 13:31:38 +0300

    Remove unnecessary files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
     base_title = "Флэшкарточкер"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
+    [base_title, page_title].compact.join(" | ")
   end
 end
