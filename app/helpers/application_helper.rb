df6c33d600a4977c365637896d5d5bead910d7ee
Misha <mikhail.krutman@gmail.com>
2015-08-19 12:32:00 +0300

    Add validates, edit some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-  
   def full_title(page_title)
     base_title = "Флэшкарточкер"
     if page_title.empty?
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true, length: { maximum: 30 }
+  validates :translated_text, presence: true, length: { maximum: 30 }
+  validates :review_date, presence: true
 end
