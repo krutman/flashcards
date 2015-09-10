ccf771c1b356d31904562d0467e42350525184d0
Misha <mikhail.krutman@gmail.com>
2015-09-10 14:35:14 +0300

    Fixes task 5 round 1

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  before_action :find_card, only: [:create]
+  
+  def new
+    @card = Card.availables_for_check.first
+  end
+  
+  def create
+    if @card.check_translation(params[:original_text])
+      flash[:success] = "Правильный ответ"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Неправильный ответ"
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def find_card
+      @card = Card.find(params[:id])
+    end
+end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,40 +1,29 @@
 class Card < ActiveRecord::Base
   before_validation :set_review_date, on: :create
+  default_scope -> { order(created_at: :desc) }
   validates :original_text, presence: true, length: { maximum: 100 }
   validates :translated_text, presence: true, length: { maximum: 150 }
   validates :review_date, presence: true
   validate  :words_not_equal
 
-  scope :by_creation_time, -> { order('created_at DESC') }
-  scope :availables_for_check, -> { where('review_date <= ?', Date.today) }
-  # scope :random_available_card, -> { availables_for_check.order('RANDOM()').first if availables_for_check.present? }
+  scope :availables_for_check, -> { where('review_date <= ?', Date.today).offset(rand(Card.count))}
   
-  def self.random_available_card
-    if availables_for_check.present?
-      availables_for_check.order('RANDOM()').first
-    else
-      nil
+  def check_translation(text)
+    if downcase_text(original_text) == downcase_text(text)
+      update_attributes(review_date: Date.today + 3.days)
     end
-  end
-  
-  def correct_word?(word)
-    self.original_text == word
-  end
-  
-  def update_review_date
-    self.update_attribute(:review_date, Date.today + 3.days)
   end
   
   private
   
     def words_not_equal
-      if downcase_word(original_text) == downcase_word(translated_text)
+      if downcase_text(original_text) == downcase_text(translated_text)
         errors.add(:original_text, "should not be equal to translated text")
       end
     end
     
-    def downcase_word(word)
-      word.mb_chars.downcase.to_s if word.present?
+    def downcase_text(text)
+      text.mb_chars.downcase.to_s if text.present?
     end
     
     def set_review_date
