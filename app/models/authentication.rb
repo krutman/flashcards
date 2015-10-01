fcef229f0eb2f60905c8fe78199d35e2d3c59868
Misha <mikhail.krutman@gmail.com>
2015-10-01 19:48:34 +0300

    Fixes before_destroy in Authentication

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,13 +1,10 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
-  before_destroy :stop_destroy_if_last_oauth_and_password_nil
+  before_destroy :stop_if_last_oauth_and_password_nil
   
   private
   
-    def stop_destroy_if_last_oauth_and_password_nil
-      user = User.find(self.user_id)
-      if user.crypted_password.nil? && user.authentications.count == 1
-        return false
-      end
+    def stop_if_last_oauth_and_password_nil
+      return !(user.crypted_password.nil? && user.authentications.count == 1)
     end
 end
